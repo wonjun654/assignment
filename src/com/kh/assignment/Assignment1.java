c051fc68b1dd1f76e8680d4e890f1682c1cdbab0
user <user@119.193.166.82>
2019-08-11 18:21:38 +0900

    psj-20190811-01

--- src/com/kh/assignment/Assignment1.java
+++ src/com/kh/assignment/Assignment1.java
@@ -1,5 +1,22 @@
 package com.kh.assignment;
 
-public class Assignment1 {
+import java.util.Scanner;
 
+public class Assignment1 {
+	
+	
+	//
+	// 사용자에게 금액을 입력받아 거스름돈을 주는 프로그램을 작성. 배열사용. 
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 int[] coinUnit = { 50000,10000,5000,1000,500, 100, 50, 10 };
+		 System.out.println("금액 입력 : ");
+		    int money = sc.nextInt();
+		    System.out.println("money=" + money);
+		    for (int i = 0; i < coinUnit.length; i++) {
+		        System.out.println(coinUnit[i]+"원: "+ money / coinUnit[i]);
+		        money %= coinUnit[i];
+		    }
+	}
 }
